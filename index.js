616c7ed800d484893bceaadda647a3f43dada9a7
Aleksandr Sadykov <Aleksandr2697@yandex.ru>
2022-06-06 11:39:37 +0300

    add popup

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let popupCloseButton = document.querySelector('.popap__close-button');
 let formElement = document.querySelector('.form__input-container');
 let profileName = document.querySelector('.profile__name')
 let profileAbout = document.querySelector('.profile__about')
-
+let submitBottom = document.querySelector('.submit__bottom')
 let profile = document.querySelector('.profile__info');
 let nameInput = document.querySelector('.form__name');
 let jobInput = document.querySelector('.form__about');
@@ -18,28 +18,33 @@ function openPopup() {
 function closePopup() {
 
     popup.classList.add('popup_hidden');
+
 }
 button.addEventListener('click', function() {
     openPopup()
 
 })
-popupCloseButton.addEventListener('click', function() {
+popupCloseButton.addEventListener('click', function(e) {
+
     if (e.target === e.currentTarget)
+
         closePopup()
 
+    e.preventDefault();
 })
 
-// function formSubmitHandler(evt) {
-//     evt.preventDefault();
 
-//     profileName.textContent = '$ { nameInput.value }';
-//     profileAbout.textContent = '$ { jobInput.value }';
+function formSubmitHandler(evt) {
+    evt.preventDefault();
 
-//     closePopup()
+    profileName.textContent = nameInput.value;
+    profileAbout.textContent = jobInput.value;
 
-// }
-// button.addEventListener('click', function() {
-//     openPopup()
+    closePopup()
 
-// })
-// submitBottom.addEventListener('click', formSubmitHandler);
+}
+button.addEventListener('click', function() {
+    openPopup()
+
+})
+submitBottom.addEventListener('click', formSubmitHandler);
