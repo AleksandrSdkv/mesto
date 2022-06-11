6a6cbe88e1cc4b67e76d01aad4a119d031efa621
Aleksandr Sadykov <Aleksandr2697@yandex.ru>
2022-06-12 01:04:29 +0300

    fx5

--- scripts/index.js
+++ scripts/index.js
@@ -3,31 +3,23 @@ let profileAbout = document.querySelector('.profile__about');
 let profileEditButton = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
 let popupCloseButton = document.querySelector('.popup__button-close');
-let formElement = document.querySelector('.popup__form-container');
-let nameInput = document.querySelector('.popup__form-input_type_name');
-let jobInput = document.querySelector('.popup__form-input_type_about');
+let formElement = document.querySelector('.form');
+let nameInput = document.querySelector('.form__input_type_name');
+let jobInput = document.querySelector('.form__input_type_about');
 
-function tooglePopup() {
-    popup.classList.toggle('popup_opened');
-}
+profileEditButton.addEventListener('click', function tooglePopup() { //объявление открытие/закрытие попапа
+    popup.classList.toggle('popup_opened'); //добавлением "паранжи"
 
+    nameInput.value = profileName.textContent; //перенос имя профия
+    jobInput.value = profileAbout.textContent; //перенос работы профия
 
+    popupCloseButton.addEventListener('click', tooglePopup); //закрытие попапа
 
-function formSubmitHandler(evt) {
-    evt.preventDefault();
-    profileName.textContent = nameInput.value;
-    profileAbout.textContent = jobInput.value;
-    tooglePopup();
-}
-
-profileEditButton.addEventListener('click', tooglePopup);
-
-popupCloseButton.addEventListener('click', tooglePopup);
-
-profileEditButton.addEventListener('click', function() {
-    nameInput.value = profileName.textContent;
-    jobInput.value = profileAbout.textContent;
+    function formSubmitHandler(evt) { //добавление нового имени и занятия
+        evt.preventDefault();
+        profileName.textContent = nameInput.value;
+        profileAbout.textContent = jobInput.value;
+        tooglePopup();
+    }
+    formElement.addEventListener('submit', formSubmitHandler);
 });
-
-
-formElement.addEventListener('submit', formSubmitHandler);
